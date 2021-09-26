7677d3ec1a2dc634abb21dd4aadcedf031e5bce3
Sam Pallis <samuel.pallis960@gmail.com>
2021-09-26 15:35:54 +0930

    Initial
    first files

---
+++ website/index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Money 2 U</title>
+    <link rel="stylesheet" href="./Styles/styles.css">
+</head>
+<body>
+
+    <?php require_once "website_header.php"; 
+    
+    
+    ?>
+    
+</body>
---
+++ website/website_header.php
@@ -0,0 +1,14 @@
+<header id="website_header">
+    <img scr="logos/logo.png" alt="Website Logo" id="Heading_Logo">
+    <h1 id="Website_Title">Money 2 U</h1>
+    <nav id="Site_Nav">
+        <ul>
+            <li><a href="index.php">home</a></li>
+            <li><a href="">products</a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">profile</a></li>
+            <li><a href="">shopping cart</a></li>
+        </ul>
+    </nav>
+
+</header>
