47bb64a2d42575854b321335ab88d7d580b0011e
Sam Pallis <samuel.pallis960@gmail.com>
2021-10-01 16:00:02 +0930

    Meeting 2 Commit

---
+++ WebDev/website/Search.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Money 2 U Search</title>
+    <link rel="stylesheet" href="./Styles/styles.css">
+    <link rel="stylesheet" href="./Styles/headerstyle.css">
+</head>
+<body>
+
+    <?php require_once "website_header.php"; 
+    
+    ?>
+
+    <form method="POST" action="">
+        <input>
+
+</input>
+        <
+    </form>
+
+
+</body>
