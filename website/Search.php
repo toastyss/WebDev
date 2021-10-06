cdd60c4b77531220c8803849a4d0750ab659be41
Sam Pallis <samuel.pallis960@gmail.com>
2021-10-06 18:59:43 +1030

    Meeting 3 Commit

---
+++ website/Search.php
@@ -0,0 +1,83 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Money 2 U Search</title>
+    <link rel="stylesheet" href="./Styles/styles.css">
+</head>
+
+<body>
+    <?php require_once "website_header.php";
+
+    ?>
+
+    <div class="search_page">
+        <aside class="sidebar">
+            <!-- Sidebar -->
+            <div id="product_filter">
+        <form id="filter_form" action="" method="GET">
+            <input id="search_bar" type="text" placeholder="Search for item" name="search"><br>
+
+            <div id="checkboxes">
+                <input id="usd_c" type="checkbox" name="currency_type" value="USD">
+                <label for="usd_c">USD</label><br> 
+                <input id="eur_c" type="checkbox" name="currency_type" value="EUR">
+                <label for="eur_c">EUR</label><br>
+                <input id="aud_c" type="checkbox" name="currency_type" value="AUD">
+                <label for="aud_c">AUD</label><br>
+            </div>
+
+            <input id="submit_btn" type="submit" value="Filter">
+        </form>
+    </div>
+        </aside>
+
+        <main class="content">
+            <!-- Search Results -->
+            <?php 
+            require_once "dbconn.php";
+
+            $sql = "SELECT currency, denomination, mint_year, quality, product_status, quantity FROM Products";
+
+            if($result = mysqli_query($conn, $sql))
+            {
+                if(mysqli_num_rows($result) > 0)
+                {
+                    echo '<p> items </p>';
+                    echo "<ol>";
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                        echo '<li>';
+                        echo '<ul>';
+                        echo '<li>', $row["currency"], "</li>";
+                        echo '<li>', $row["denomination"], "</li>";
+                        echo '<li>', $row["mint_year"], "</li>";
+                        echo '<li>', $row["quality"], "</li>";
+                        echo '<li>', $row["product_status"], "</li>";
+                        echo '<li>', $row["quantity"], "</li>";
+                        echo '</ul>';
+                        echo '</li>';
+                    }
+                    echo "</ol>";
+                }
+                else 
+                {
+                    echo "<p> no results found </p>";
+                }
+            }
+            else
+            {
+                echo "<p> an error occured </p>";
+            }
+            
+            mysqli_close($conn);
+
+            ?>
+            
+        </main>
+    </div>
+
+    
+</body>
---
+++ website/website_header.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="./Styles/headerstyle.css">
+</head>
+
+
+<header id="website_header">
+    <a href="index.php"><img src="assets/images/Logo_Black.png" alt="Website Logo" id="Heading_Logo"></a>
+    <nav id="Site_Nav">
+        <ul>
+            <li><a href="Search.php">Search</a></li>
+            <li><a href="">Account</a></li>
+            <li><a href="">Checkout</a></li>
+        </ul>
+        <div style="clear: both"></div>
+    </nav>
+    <div style="clear: both"></div>
+
+</header>
